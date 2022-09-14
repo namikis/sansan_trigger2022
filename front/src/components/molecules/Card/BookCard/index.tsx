006a1249134f625e2cc593f73bf3a0e23d2167ff
mahiro72 <mahiro.yoshie@gmail.com>
2022-09-14 17:44:48 +0900

    add mock data

--- front/src/components/molecules/Card/BookCard/index.tsx
+++ front/src/components/molecules/Card/BookCard/index.tsx
@@ -5,16 +5,22 @@ import MediumText from "../../../atoms/Text/MediumText"
 import SmallText from "../../../atoms/Text/SmallText"
 import BookImage from "../../../atoms/Image/BookImage"
 
-const index:React.FC<{index:number}> = ({index}) => {
+type bookType = {
+  title:string;
+  description:string;
+  imgUrl:string;
+}
+
+const index:React.FC<{index:number,book:bookType}> = ({index,book}) => {
   return (
     <div className="p-2 lg:w-1/4 md:w-1/3 my-2">
         <Link to={`/book/${index}`}>
         <div className="h-full bg-brown-base hover:bg-brown-base-shadow rounded-lg overflow-hidden shadow-lg">
-            <BookImage />
+            <BookImage imgUrl={book.imgUrl}/>
             <div className='border border-gray-200 my-1 mx-1'></div>
-            <div className="p-6 bg-brown-llight">
-                <MediumText text={"入門 Python3"} color='text-brown-dark' margin={false} />
-                <SmallText text={"sub sub sub"} color='text-brown-dark' margin={false} />
+            <div className="p-6 bg-brown-llight h-full">
+                <MediumText text={book.title} color='text-brown-dark' margin={false} />
+                <SmallText text={book.description} color='text-brown-dark' margin={false} />
             </div>
         </div>
         </Link>
